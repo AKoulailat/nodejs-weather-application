9ba069336c606d5fc8464dc8f26864760561e8b1
Abdel Hady Koulailat <abdel.koulailat@broadbean.com>
2020-04-17 19:08:32 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const messageTwo = document.querySelector('#message-2');
 weatherFrom.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+  fetch(`/weather?address=${search.value}`).then((response) => {
     response.json().then((data) => {
       messageOne.textContent = '';
       messageTwo.textContent = '';
